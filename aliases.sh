1fa56fea51183539e78fd3619d90d1926c9fe042
Tom Vincent <git@tlvince.com>
2013-05-19 15:56:26 +0800

    Add aliases

---
+++ aliases.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Package management
+alias repkg='makepkg -efis'
+alias pinstall='sudo pacman -S'
+alias uninstall='sudo pacman -Rsn'
+alias funinstall='sudo pacman -Rsndd'
+alias aur='raury'
