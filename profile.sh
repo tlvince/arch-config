e1ea39fd10467a40b255bfa8e06a19556a3270cd
Tom Vincent <git@tlvince.com>
2013-06-01 14:23:43 +0800

    Move keychain into generic profile

--- profile.sh
+++ profile.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-have() { which "$1" &>/dev/null; }
-
-# Start {ssh,gpg}-agent
-have keychain && eval $(keychain --eval --quiet)
-
 # Start X
 if [ -z "$DISPLAY" ] && [ $(tty) = "/dev/tty1" ]; then
   # Fix login security hole (^c)
